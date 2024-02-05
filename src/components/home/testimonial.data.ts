23a9461bdeb5a5335ced8eff493559c3f5f17869
Haldun Mammadzada <haldun.mammadzada@gmail.com>
2024-02-06 01:50:36 +0400

    ts-error

--- src/components/home/testimonial.data.ts
+++ src/components/home/testimonial.data.ts
@@ -4,21 +4,25 @@ export const data: Array<Testimonial> = [
   {
     id: 1,
     name: 'Estonia',
+    // city: 'Tallin',
     photo: '1.png',
   },
   {
     id: 2,
     name: 'Finland',
+    // city: 'Helsinki',
     photo: '2.png',
   },
   {
     id: 3,
     name: 'Latvia',
+    // city: 'Riga',
     photo: '3.png',
   },
   {
     id: 4,
     name: 'Lithuania',
+    // city: 'Vilnius',
     photo: '4.png',
   },
 ]
